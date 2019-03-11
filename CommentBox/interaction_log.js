a08526b0d1e8adf5b51401a3d16d229b2e097757
Enamul Hoque <enamul@cs.ubc.ca>
2019-03-11 00:52:01 -0400

    fix userid

--- CommentBox/interaction_log.js
+++ CommentBox/interaction_log.js
@@ -21,3 +21,13 @@ function logInteraction(str)
         request.send(str);
     }
 }
+function makeRandomId()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 7; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
